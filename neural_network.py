5b3708e7258b02ccf67f879a48235f88b51f58b6
Keval Khara <kevalkhara95@gmail.com>
2018-01-14 14:20:30 -0500

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -26,11 +26,70 @@ def plot_decision_boundary(pred_func):
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5 
     h = 0.01 
     # Generate a grid of points with distance h between them 
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h)) 
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    
     # Predict the function value for the whole gid 
     Z = pred_func(np.c_[xx.ravel(), yy.ravel()]) 
-    Z = Z.reshape(xx.shape) 
+    Z = Z.reshape(xx.shape)
+    
     # Plot the contour and training examples 
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral) 
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral) 
+
+# Plot the decision boundary 
+plot_decision_boundary(lambda x: clf.predict(x)) 
+plt.title("Logistic Regression") 
+ 
+num_examples = len(X)    # training set size 
+nn_input_dim = 2         # input layer dimensionality 
+nn_output_dim = 2        # output layer dimensionality 
+ 
+# Gradient descent parameters
+epsilon = 0.01           # learning rate for gradient descent 
+reg_lambda = 0.01        # regularization strength 
+ 
+# Function to evaluate the total loss on the dataset 
+def calculate_loss(model): 
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2'] 
+    # Forward propagation to calculate our predictions 
+    z1 = X.dot(W1) + b1 
+    a1 = np.tanh(z1) 
+    z2 = a1.dot(W2) + b2 
+    exp_scores = np.exp(z2) 
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    
+    # Calculating the loss 
+    corect_logprobs = -np.log(probs[range(num_examples), y]) 
+    data_loss = np.sum(corect_logprobs)
+    
+    # Add regulatization term to loss (optional) 
+    data_loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2))) 
+    return 1./num_examples * data_loss 
+ 
+# Function to predict an output (0 or 1) 
+def predict(model, x): 
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2'] 
+    # Forward propagation 
+    z1 = x.dot(W1) + b1 
+    a1 = np.tanh(z1) 
+    z2 = a1.dot(W2) + b2 
+    exp_scores = np.exp(z2) 
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) 
+    return np.argmax(probs, axis=1) 
+ 
+# This function learns parameters for the neural network and returns the model. 
+# - nn_hdim: Number of nodes in the hidden layer 
+# - num_passes: Number of passes through the training data for gradient descent 
+# - print_loss: If True, print the loss every 1000 iterations 
+def build_model(nn_hdim, num_passes=20000, print_loss=False): 
+ 
+    # Initialize the parameters to random values. We need to learn these. 
+    np.random.seed(0) 
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim) 
+    b1 = np.zeros((1, nn_hdim)) 
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim) 
+    b2 = np.zeros((1, nn_output_dim)) 
+ 
+    # This is what we return at the end 
+    model = {} 
 
