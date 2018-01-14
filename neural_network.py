f44ea44220a681494ea05e027d37377f40d23b63
Keval Khara <kevalkhara95@gmail.com>
2018-01-14 14:26:25 -0500

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -97,3 +97,55 @@ def build_model(nn_hdim, num_passes=20000, print_loss=False):
 
     model = {} 
 
+    # Gradient descent for each batch 
+    for i in range(0, num_passes): 
+        # Forward propagation 
+        z1 = X.dot(W1) + b1 
+        a1 = np.tanh(z1) 
+        z2 = a1.dot(W2) + b2 
+        exp_scores = np.exp(z2) 
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) 
+ 
+        # Backpropagation 
+        delta3 = probs 
+        delta3[range(num_examples), y] -= 1 
+        dW2 = (a1.T).dot(delta3) 
+        db2 = np.sum(delta3, axis=0, keepdims=True) 
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2)) 
+        dW1 = np.dot(X.T, delta2) 
+        db1 = np.sum(delta2, axis=0) 
+ 
+        # Add regularization terms (b1 and b2 don't have regularization terms) 
+        dW2 += reg_lambda * W2 
+        dW1 += reg_lambda * W1 
+ 
+        # Gradient descent parameter update 
+        W1 += -epsilon * dW1 
+        b1 += -epsilon * db1 
+        W2 += -epsilon * dW2 
+        b2 += -epsilon * db2 
+ 
+        # Assign new parameters to the model 
+        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2} 
+ 
+        # Optionally print the loss
+        if print_loss and i % 1000 == 0: 
+          print("Loss after iteration %i: %f" %(i, calculate_loss(model))) 
+ 
+    return model 
+ 
+# Build a model with a 3-dimensional hidden layer 
+model = build_model(3, print_loss=True) 
+ 
+# Plot the decision boundary 
+plot_decision_boundary(lambda x: predict(model, x)) 
+plt.title("Decision Boundary for hidden layer size 3") 
+  
+plt.figure(figsize=(16, 32)) 
+hidden_layer_dimensions = [1, 2, 3, 4, 5, 20, 50] 
+for i, nn_hdim in enumerate(hidden_layer_dimensions): 
+    plt.subplot(5, 2, i+1) 
+    plt.title('Hidden Layer size %d' % nn_hdim) 
+    model = build_model(nn_hdim) 
+    plot_decision_boundary(lambda x: predict(model, x)) 
+plt.show()
